f3923cf6f87c9de7e2005cf04a2c5264c4d54782
codegeek004 <yashvaishnav1404@gmail.com>
2025-11-05 17:10:02 +0530

    added domain url in allowed origins list

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI(title="Smart Resume Screener API")
 # CORS 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "https://codemos-services.co.in"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
